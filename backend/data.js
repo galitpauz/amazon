03b3546de9953e71314ee0eb664c08c3339a3297
galitpauz <45431987+galitpauz@users.noreply.github.com>
2021-06-22 18:41:49 +0300

    amazon app create product screen

---
+++ backend/data.js
@@ -0,0 +1,38 @@
+export default {
+    products: [
+        {
+            _id:'1',
+            name:'Slim Shirt',
+            category:'Shirts',
+            image:'/images/d1.jpg',
+            brand: 'Nike',
+            price:50,
+            rating:4.5,
+            numReviews: 9,
+            countInStock: 6,
+        },
+        {
+            _id:'2',
+            name:'Fit Shirt',
+            category:'Shirts',
+            image:'/images/d1.jpg',
+            brand:'Nike',
+            price: 60,
+            rating: 5,
+            numReviews: 8,
+            countInStock: 3,
+        },
+        {
+            _id:'3',
+            name:'Best Pants',
+            category:'Pants',
+            image:'/images/d1.jpg',
+            brand:'Nike',
+            price: 70,
+            rating: 4.2,
+            numReviews: 10,
+            countInStock: 5,
+
+        },
+    ]
+}
